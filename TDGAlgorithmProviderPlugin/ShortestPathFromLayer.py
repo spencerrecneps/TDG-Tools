4eb3b40947ac5963f4e8dcd336fc3d623dda8b01
Spencer Gardner <spencergardner@gmail.com>
2015-10-17 21:49:46 -0500

    shortest path summary and routes

--- TDGAlgorithmProviderPlugin/ShortestPathFromLayer.py
+++ TDGAlgorithmProviderPlugin/ShortestPathFromLayer.py
@@ -33,6 +33,7 @@ from processing.core.GeoAlgorithm import GeoAlgorithm
 from processing.core.GeoAlgorithmExecutionException import GeoAlgorithmExecutionException
 from processing.core.parameters import ParameterVector
 from processing.core.parameters import ParameterString
+from processing.core.parameters import ParameterBoolean
 from processing.core.parameters import ParameterNumber
 from processing.core.parameters import ParameterTableField
 from processing.core.parameters import ParameterSelection
@@ -59,7 +60,12 @@ class ShortestPathFromLayer(GeoAlgorithm):
     DESTINATIONS_LAYER = 'DESTINATIONS_LAYER'
     VERT_ID_FIELD = 'VERT_ID_FIELD'
     STRESS = 'STRESS'
-    OUT_LAYER = 'OUT_LAYER'
+    KEEP_RAW = 'KEEP_RAW'
+    RAW_LAYER = 'RAW_LAYER'
+    KEEP_ROUTES = 'KEEP_ROUTES'
+    ROUTES_LAYER = 'ROUTES_LAYER'
+    KEEP_SUMS = 'KEEP_SUMS'
+    SUMS_LAYER = 'SUMS_LAYER'
 
 
     def defineCharacteristics(self):
@@ -76,31 +82,85 @@ class ShortestPathFromLayer(GeoAlgorithm):
 
         # Input roads layer. Must be line type
         # It is a mandatory (not optional) one, hence the False argument
-        self.addParameter(ParameterVector(self.ROADS_LAYER,
-            self.tr('Roads layer (must have a network built)'),
-            [ParameterVector.VECTOR_TYPE_LINE], optional=False))
+        self.addParameter(
+            ParameterVector(
+                self.ROADS_LAYER,
+                self.tr('Roads layer (must have a network built)'),
+                [ParameterVector.VECTOR_TYPE_LINE],
+                optional=False
+            )
+        )
 
         # Input roads layer. Must be line type
         # It is a mandatory (not optional) one, hence the False argument
-        self.addParameter(ParameterVector(self.DESTINATIONS_LAYER,
-            self.tr('Destinations layer (must have vertex IDs)'),
-            [ParameterVector.VECTOR_TYPE_POINT], optional=False))
+        self.addParameter(
+            ParameterVector(
+                self.DESTINATIONS_LAYER,
+                self.tr('Destinations layer (must have vertex IDs)'),
+                [ParameterVector.VECTOR_TYPE_POINT],
+                optional=False
+            )
+        )
 
         # Field with vertex IDs
         # Required
-        self.addParameter(ParameterTableField(self.VERT_ID_FIELD,
-            self.tr('Field containing the network vertex IDs'),
-            parent=self.DESTINATIONS_LAYER,
-            datatype = ParameterTableField.DATA_TYPE_NUMBER,
-            optional=False))
+        self.addParameter(
+            ParameterTableField(
+                self.VERT_ID_FIELD,
+                self.tr('Field containing the network vertex IDs'),
+                parent=self.DESTINATIONS_LAYER,
+                datatype = ParameterTableField.DATA_TYPE_NUMBER,
+                optional=False
+            )
+        )
 
         # Max stress
-        self.addParameter(ParameterNumber(self.STRESS,
-            self.tr('Maximum allowable traffic stress (leave at 0 to ignore)'),
-            minValue=0,maxValue=4))
+        self.addParameter(
+            ParameterNumber(
+                self.STRESS,
+                self.tr('Maximum allowable traffic stress (leave at 0 to ignore)'),
+                minValue=0,maxValue=4
+            )
+        )
 
-        # Output layer
-        self.addOutput(OutputVector(self.OUT_LAYER, self.tr('Shortest paths')))
+        # Keep raw layer?
+        self.addParameter(
+            ParameterBoolean(
+                self.KEEP_RAW,
+                self.tr('Keep raw shortest path layer?'), default=True
+            )
+        )
+
+        # Output raw layer
+        self.addOutput(
+            OutputVector(self.RAW_LAYER, self.tr('Raw shortest path layer'))
+        )
+
+        # Keep routes layer?
+        self.addParameter(
+            ParameterBoolean(
+                self.KEEP_ROUTES,
+                self.tr('Keep shortest path routes layer?'), default=True
+            )
+        )
+
+        # Output routes layer
+        self.addOutput(
+            OutputVector(self.ROUTES_LAYER, self.tr('Shortest path routes layer'))
+        )
+
+        # Keep sum layer?
+        self.addParameter(
+            ParameterBoolean(
+                self.KEEP_SUMS,
+                self.tr('Keep path summary layer?'), default=True
+            )
+        )
+
+        # Output sum layer
+        self.addOutput(
+            OutputVector(self.SUMS_LAYER, self.tr('Shortest path summary layer'))
+        )
 
 
     def processAlgorithm(self, progress):
@@ -112,20 +172,38 @@ class ShortestPathFromLayer(GeoAlgorithm):
             self.getParameterValue(self.DESTINATIONS_LAYER))
         vertIdField = self.getParameterValue(self.VERT_ID_FIELD)
         stress = self.getParameterValue(self.STRESS)
+        keepRaw = self.KEEP_RAW
+        keepRoutes = self.KEEP_ROUTES
+        keepSums = self.KEEP_SUMS
 
-        # build the output layer
-        fields = QgsFields()
-        fields.append(QgsField('path_id', QVariant.Int))
-        fields.append(QgsField('sequence', QVariant.Int))
-        fields.append(QgsField('from_vert', QVariant.Int))
-        fields.append(QgsField('to_vert', QVariant.Int))
-        fields.append(QgsField('int_id', QVariant.Int))
-        fields.append(QgsField('int_cost', QVariant.Int))
-        fields.append(QgsField('road_id', QVariant.Int))
-        fields.append(QgsField('road_cost', QVariant.Int))
-        fields.append(QgsField('cmtve_cost', QVariant.Int))
-        writer = self.getOutputFromName(self.OUT_LAYER).getVectorWriter(
-            fields, QGis.WKBLineString, roadsLayer.crs())
+        # build the raw output layer
+        if keepRaw:
+            rawFields = QgsFields()
+            rawFields.append(QgsField('path_id', QVariant.Int))
+            rawFields.append(QgsField('sequence', QVariant.Int))
+            rawFields.append(QgsField('from_vert', QVariant.Int))
+            rawFields.append(QgsField('to_vert', QVariant.Int))
+            rawFields.append(QgsField('int_id', QVariant.Int))
+            rawFields.append(QgsField('int_cost', QVariant.Int))
+            rawFields.append(QgsField('road_id', QVariant.Int))
+            rawFields.append(QgsField('road_cost', QVariant.Int))
+            rawFields.append(QgsField('cmtve_cost', QVariant.Int))
+            rawWriter = self.getOutputFromName(self.RAW_LAYER).getVectorWriter(
+                rawFields, QGis.WKBLineString, roadsLayer.crs())
+        if keepRoutes:
+            routeFields = QgsFields()
+            routeFields.append(QgsField('path_id', QVariant.Int))
+            routeFields.append(QgsField('from_vert', QVariant.Int))
+            routeFields.append(QgsField('to_vert', QVariant.Int))
+            routeFields.append(QgsField('cmtve_cost', QVariant.Int))
+            routeWriter = self.getOutputFromName(self.ROUTES_LAYER).getVectorWriter(
+                routeFields, QGis.WKBLineString, roadsLayer.crs())
+        if keepSums:
+            sumFields = QgsFields()
+            sumFields.append(QgsField('road_id', QVariant.Int))
+            sumFields.append(QgsField('use_count', QVariant.Int))
+            sumWriter = self.getOutputFromName(self.SUMS_LAYER).getVectorWriter(
+                sumFields, QGis.WKBLineString, roadsLayer.crs())
         progress.setPercentage(2)
 
         # establish db connection
@@ -179,10 +257,11 @@ class ShortestPathFromLayer(GeoAlgorithm):
         # set up dictionary of road_ids with their geoms
         roads = dict()
         for feat in vector.features(roadsLayer):
-            #roads[feat['road_id']] = feat.geometry()
-            f = QgsFeature(fields)
-            f.setGeometry(feat.geometry())
-            roads[feat['road_id']] = f
+            g = QgsGeometry(feat.geometry())
+            f = QgsFeature(sumFields)
+            f.setGeometry(g)
+            f.setAttribute(0,feat['road_id'])
+            roads[feat['road_id']] = {'geom': g, 'count': 0, 'feat': f}
 
         # loop through each destination and get shortest routes to all others
         try:
@@ -191,12 +270,23 @@ class ShortestPathFromLayer(GeoAlgorithm):
             for fromVert in vertIds:
                 for toVert in vertIds:
                     if not fromVert == toVert:
+                        # set counts
                         pairCount += 1
                         seq = 0
                         cost = 0
                         if pairCount % 1000 == 0:
                             progress.setInfo('Shortest path for pair %i of %i'
                                     % (pairCount, vertPairCount))
+
+                        # set feature for route output
+                        routeFeat = None
+                        if keepRoutes:
+                            routeFeat = QgsFeature(routeFields)
+                            routeFeat.setAttribute(0,pairCount) #path_id
+                            routeFeat.setAttribute(1,fromVert) #from_vert
+                            routeFeat.setAttribute(2,toVert) #to_vert
+
+                        # check the path and iterate through it
                         if nx.has_path(DG,source=fromVert,target=toVert):
                             shortestPath = nx.shortest_path(DG,
                                                             source=fromVert,
@@ -214,6 +304,7 @@ class ShortestPathFromLayer(GeoAlgorithm):
                                     if not roadId:
                                         continue       #skip if this isn't a road link
                                     seq += 1
+                                    roads[roadId]['count'] += 1
                                     v3 = None
                                     if i < len(shortestPath) - 2:
                                         v3 = shortestPath[i+2]
@@ -226,30 +317,47 @@ class ShortestPathFromLayer(GeoAlgorithm):
                                     cost += linkCost
                                     cost += intCost
 
-                                    # create the new feature
-                                    if roadId in roads and roads.get(roadId):
-                                        #f = QgsFeature(fields)
-                                        f = roads.get(roadId)
-                                        f.setAttribute(0,pairCount) #path_id
-                                        f.setAttribute(1,seq) #sequence
-                                        f.setAttribute(2,fromVert) #from_vert
-                                        f.setAttribute(3,toVert) #to_vert
-                                        f.setAttribute(4,DG.node[v2]['int_id']) #int_id
-                                        f.setAttribute(5,intCost) #int_cost
-                                        f.setAttribute(6,roadId) #road_id
-                                        f.setAttribute(7,linkCost) #road_cost
-                                        f.setAttribute(8,cost) #cmtve_cost
-                                    #if roadId in roads and roads.get(roadId):
-                                        #pass
-                                        #f.setGeometry(roads.get(roadId))
-                                        #f.setGeometry(roads.get(roadId).geometry())
+                                    # create the new features
+                                    if roadId in roads and roads.get(roadId).get('geom'):
+                                        geom = roads.get(roadId).get('geom')
+                                        if keepRaw:
+                                            rawFeat = QgsFeature(rawFields)
+                                            rawFeat.setAttribute(0,pairCount) #path_id
+                                            rawFeat.setAttribute(1,seq) #sequence
+                                            rawFeat.setAttribute(2,fromVert) #from_vert
+                                            rawFeat.setAttribute(3,toVert) #to_vert
+                                            rawFeat.setAttribute(4,DG.node[v2]['int_id']) #int_id
+                                            rawFeat.setAttribute(5,intCost) #int_cost
+                                            rawFeat.setAttribute(6,roadId) #road_id
+                                            rawFeat.setAttribute(7,linkCost) #road_cost
+                                            rawFeat.setAttribute(8,cost) #cmtve_cost
+                                            rawFeat.setGeometry(geom)
+                                            rawWriter.addFeature(rawFeat)
+                                            del rawFeat
+                                        if keepRoutes:
+                                            routeFeat.setAttribute(3,cost) #cmtve_cost
+                                            if not routeFeat.constGeometry():
+                                                routeFeat.setGeometry(geom)
+                                            else:
+                                                routeFeat.setGeometry(
+                                                    geom.combine(routeFeat.constGeometry())
+                                                )
+                                            routeWriter.addFeature(routeFeat)
+                                        if keepSums:
+                                            sumFeat = roads.get(roadId).get('feat')
+                                            useCount = roads.get(roadId).get('count')
+                                            sumFeat.setAttribute(1,useCount) #use_count
 
-                                    # NEED TO ADD ATTRIBUTES TO NEW FEATURE AND THEN
-                                    writer.addFeature(f)
-                                    del f
-
+                        del routeFeat
                         progress.setPercentage(10 + 90*pairCount/vertPairCount)
+
+            for roadId, r in roads.iteritems():
+                if r.get('count') > 0:
+                    sumWriter.addFeature(r.get('feat'))
+
         except Exception, e:
             raise GeoAlgorithmExecutionException('Uncaught error: %s' % e)
 
-        del writer
+        del rawWriter
+        del routeWriter
+        del sumWriter
