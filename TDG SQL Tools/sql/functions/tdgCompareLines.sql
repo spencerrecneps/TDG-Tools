8260bc573da46ffac218980c7e521718472ef5d3
Spencer <spencergardner@gmail.com>
2016-10-19 17:17:15 -0500

    simplify line comparison

--- TDG SQL Tools/sql/functions/tdgCompareLines.sql
+++ TDG SQL Tools/sql/functions/tdgCompareLines.sql
@@ -8,25 +8,20 @@ RETURNS FLOAT AS $func$
 DECLARE
     base_length FLOAT;
     num_points INTEGER;
-    comp_length FLOAT;
     avg_dist FLOAT;
 
 BEGIN
     base_length := ST_Length(base_geom_);
     num_points := CEILING(base_length::FLOAT/seg_length_);
 
-    CREATE TEMP TABLE tmp_linecompare (
-        id SERIAL PRIMARY KEY,
-        geom geometry(point)
-    )
-    ON COMMIT DROP;
-
-    INSERT INTO tmp_linecompare (geom)
-    SELECT  ST_LineInterpolatePoint(base_geom_,i::FLOAT * seg_length_ / base_length)
-    FROM    generate_series(0,num_points) i
-    WHERE   i * seg_length_ <= base_length;
-
-    avg_dist := AVG(ST_Distance(geom,comp_geom_)) FROM tmp_linecompare;
+    avg_dist := AVG(
+                    ST_Distance(
+                        ST_LineInterpolatePoint(base_geom_,i::FLOAT * seg_length_ / base_length),
+                        comp_geom_
+                    )
+                )
+    FROM        generate_series(0,num_points) i
+    WHERE       i * seg_length_ <= base_length;
 
     RETURN avg_dist;
 END $func$ LANGUAGE plpgsql;
